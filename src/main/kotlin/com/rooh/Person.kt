adbfbd61b68adda9ccfe7caf9efc4b5b1cd2b420
Roohi Singh <singhroohi184@gmail.com>
2021-01-13 23:20:24 -0500

    Initial commit

---
+++ src/main/kotlin/com/rooh/Person.kt
@@ -0,0 +1,7 @@
+package com.rooh
+
+class Person (var name : String) {
+   fun display(){
+       print("The name of the person is ${name}")
+   }
+}
---
+++ src/main/kotlin/myFirstfile.kt
@@ -0,0 +1,11 @@
+import com.rooh.Person
+
+fun main(args:Array<String>){
+var name:String
+name = "Steve"
+
+    var personObj = Person("Steve")
+    personObj.display()
+}
+
+
